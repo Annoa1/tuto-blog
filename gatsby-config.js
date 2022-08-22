df7b4bf1202efc482568b4de2f773ca57df147ba
Elyse Cordeau <elyse.cordeau@goto-games.com>
2022-08-22 15:20:39 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: `TutoBlog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://tutoblogmain.gtsb.io/`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
